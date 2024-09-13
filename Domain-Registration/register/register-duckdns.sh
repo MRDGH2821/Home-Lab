aaf9e40984ab1b11f8b67b5a65ecc393c37d879e
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-13 11:08:20 +0530

    fix(dns): :rotating_light: wrap double quotes with wildcard
    This solves globbing linter warning of shellcheck

--- Domain-Registration/register/register-duckdns.sh
+++ Domain-Registration/register/register-duckdns.sh
@@ -3,10 +3,10 @@
 set -e
 
 /lego \
-	--accept-tos \
-	--dns duckdns \
-	--dns.resolvers "1.1.1.1:53, 8.8.8.8:53" \
-	--domains *."${DUCKDNS_DOMAIN}" \
-	--domains "${DUCKDNS_DOMAIN}" \
-	--email "${LETS_ENCRYPT_EMAIL}" \
-	run
+  --accept-tos \
+  --dns duckdns \
+  --dns.resolvers "1.1.1.1:53, 8.8.8.8:53" \
+  --domains "*.${DUCKDNS_DOMAIN}" \
+  --domains "${DUCKDNS_DOMAIN}" \
+  --email "${LETS_ENCRYPT_EMAIL}" \
+  run
--- Domain-Registration/register/register-localcert.sh
+++ Domain-Registration/register/register-localcert.sh
@@ -6,7 +6,7 @@ set -e
   --accept-tos \
   --dns acme-dns \
   --dns.resolvers "1.1.1.1:53, 8.8.8.8:53" \
-  --domains *."${ACMEDNS_FULLDOMAIN}" \
+  --domains "*.${ACMEDNS_FULLDOMAIN}" \
   --domains "${ACMEDNS_FULLDOMAIN}" \
   --email "${LETS_ENCRYPT_EMAIL}" \
   --server https://acme-staging-v02.api.letsencrypt.org/directory \
