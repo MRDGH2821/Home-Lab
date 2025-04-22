3ead635d7e6d168f6d0969455b2df83459a3b386
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-04-22 16:59:12 +1000

    chore: :hammer: add rclone dir making script

---
+++ rclone-mkdir.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This script creates a directory on a remote storage using rclone.
+
+# Usage: rclone-mkdir.sh <dir_name>
+
+create_directory() {
+  local dir_name="$1"
+  rclone mkdir pcloud-homelab-data:"$dir_name"
+}
+
+preconfigured_paths=("/gamevault/var/lib/postgresql/data" "/gamevault/media" "/gamevault/files")
+
+if [ -z "$1" ]; then
+  echo "No path provided. Using preconfigured paths."
+  echo "The following paths will be created:"
+  for path in "${preconfigured_paths[@]}"; do
+    echo "$path"
+  done
+  read -rp "Do you want to proceed with creating the preconfigured paths? (y/n): " confirm
+  if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
+    echo "Operation canceled."
+    exit 0
+  fi
+  for path in "${preconfigured_paths[@]}"; do
+    echo "creating path: $path"
+    create_directory "$path"
+  done
+else
+  echo "creating path: $1"
+  create_directory "$1"
+fi
