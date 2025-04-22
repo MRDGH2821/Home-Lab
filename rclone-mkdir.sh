4d3a3548be56df4ec478d509e9cc0665cc06aba1
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-04-22 21:21:16 +1000

    fix: :rotating_light: fix linter warnings

--- rclone-mkdir.sh
+++ rclone-mkdir.sh
@@ -6,25 +6,25 @@
 
 create_directory() {
   local dir_name="$1"
-  rclone mkdir pcloud-homelab-data:"$dir_name"
+  rclone mkdir pcloud-homelab-data:"${dir_name}"
 }
 
 preconfigured_paths=("/pgadmin4/var/lib/pgadmin" "/karakeep/data" "/meilisearch/meili_data" "/gamevault/var/lib/postgresql/data" "/gamevault/media" "/gamevault/files")
 
-if [ -z "$1" ]; then
+if [[ -z $1 ]]; then
   echo "No path provided. Using preconfigured paths."
   echo "The following paths will be created:"
   for path in "${preconfigured_paths[@]}"; do
-    echo "$path"
+    echo "${path}"
   done
   read -rp "Do you want to proceed with creating the preconfigured paths? (y/n): " confirm
-  if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
+  if [[ ${confirm} != "y" && ${confirm} != "Y" ]]; then
     echo "Operation canceled."
     exit 0
   fi
   for path in "${preconfigured_paths[@]}"; do
-    echo "creating path: $path"
-    create_directory "$path"
+    echo "creating path: ${path}"
+    create_directory "${path}"
   done
 else
   echo "creating path: $1"
